34941ce0172e77dc2a2664f686afdebeeaf07462
Fei <fei-fei.zheng@ibm.com>
2021-01-11 11:00:16 -0500

    fix button error

--- src/components/sign-in.jsx
+++ src/components/sign-in.jsx
@@ -51,7 +51,7 @@ export const SignIn = () => {
 				
 				<div className="buttons">
 					<CustomButton type="submit"> Sign in </CustomButton>
-					<CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+					<CustomButton type="button" onClick={signInWithGoogle} isGoogleSignIn>
 						Sign in with Google
 					</CustomButton>
 				</div>
