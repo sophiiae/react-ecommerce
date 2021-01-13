a3b6b8b52c9eda540eaf2ed72dab53871011af88
Fei <fei-fei.zheng@ibm.com>
2021-01-13 15:42:09 -0500

    add redux to header and app components

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { Route, Switch } from 'react-router-dom';
+import { connect } from 'react-redux';
+
 import { Homepage, ShopPage, SignInAndSignUp } from './pages/index';
 import { Header } from './components/index';
-
 import './App.css';
 import './scss/styles.scss';
 
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { setCurrentUser } from './redux/user/user.actions';
 
-function App() {
-	const [state, setstate] = useState({ currentUser: null });
-
+function App(props) { 
 	useEffect(() => {
 		auth.onAuthStateChanged(async userAuth => {
 			// store user information if user has signedn in
@@ -18,22 +18,20 @@ function App() {
 				const userRef = await createUserProfileDocument(userAuth);
 
 				userRef.on('value', (snapshot) => {
-					setstate({
-						currentUser: {
-							id: snapshot.key, 
-							...snapshot.val()
-						}
+					props.setCurrentUser({
+						id: snapshot.key, 
+						...snapshot.val()
 					});
 				})
 			}
 
-			setstate({ currentUser: userAuth });
+			props.setCurrentUser(userAuth); // pass update value
 		})
-	}, []);
+	}, [props]);
 
 	return (
 		<div>
-			<Header currentUser={state.currentUser} />
+			<Header />
 			<Switch>
 				<Route exact path='/' component={Homepage} />
 				<Route path='/shop' component={ShopPage} />
@@ -43,4 +41,8 @@ function App() {
 	)
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+	setCurrentUser: user => dispatch(setCurrentUser(user))
+})
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../assets/crown.svg';
 import { auth } from '../firebase/firebase.utils';
 
-export const Header = ({ currentUser }) => (
+const HeaderComponent = ({ currentUser }) => (
 	<div className="header">
 		<Link className='logo-container' to="/">
 			<Logo className='logo' />
@@ -27,3 +28,9 @@ export const Header = ({ currentUser }) => (
 		</div>
 	</div>
 )
+
+const mapStateToProps = state => ({
+	currentUser: state.user.currentUser
+})
+
+export const Header = connect(mapStateToProps)(HeaderComponent);
