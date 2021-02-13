27722434bcf2a96de8b7a8c69c6b92ffc9801dd9
Fei <fei-fei.zheng@ibm.com>
2021-02-13 15:12:06 -0500

    use memorized selectors for cart items

--- src/components/cart-dropdown.jsx
+++ src/components/cart-dropdown.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
-import { CustomButton } from './custom-button';
+import { connect } from 'react-redux';
 
-export const CartDropdown = () => (
+import { CustomButton } from './custom-button';
+import { CartItem } from './cart-item';
+import { selectCartItems } from '../redux/cart/cart.selectors'
+
+const CartDropdown = ({ cartItems }) => (
 	<div className="cart-dropdown">
 		<div className="cart-items">
+			{
+				cartItems.map(cartItem => <CartItem key={cartItem.id} item={cartItem} />)
+			}
 		</div>
 		<CustomButton>GO TO CHECKOUT</CustomButton>
 	</div>
 )
+
+const mapStateToProps = state => ({
+	cartItems: selectCartItems(state)
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -4,7 +4,7 @@ import { connect } from 'react-redux';
 
 import { ReactComponent as Logo } from '../assets/crown.svg';
 import { auth } from '../firebase/firebase.utils';
-import { CartDropdown } from './index';
+import CartDropdown from './cart-dropdown';
 import CartIcon from './cart-icon';
 
 const HeaderComponent = ({ currentUser, hidden }) => (
